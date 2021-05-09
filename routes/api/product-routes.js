93fb6b6fd4d201fad416111a66936d50fdca466f
itsnestor <nestor.dejoya.campaner@gmail.com>
2021-05-08 22:09:43 -0400

    added functionality to product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,27 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll(
+      {include: [Category, {model: Tag, through: ProductTag}]}
+    );
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id,
+      { include: [Category, {model: Tag, through: ProductTag}]}
+    );
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -91,6 +106,16 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
